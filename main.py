27e66af65b05850ca515ac1744fbebba0ef2110d
Dominick Caponi <d.caponi1@gmail.com>
2024-02-29 21:18:11 -0800

    fix subtitle whole movie

--- main.py
+++ main.py
@@ -15,7 +15,12 @@ def write(clip: CompositeVideoClip | VideoClip | VideoFileClip, out, temp_audio=
     # Thats bad because we do an incremental write for each scene (0.mp3, 1.mp3) which means overlap
     # Overlap is bad because moviepy wont make another temp audio file if the 1.mp3 from a previous movie exists so you get
     # weird audio bugs if you don't do this.
-    clip.write_videofile(out, threads=threads, temp_audiofile=temp_audio)
+    clip.write_videofile(
+        out,
+        threads=threads,
+        temp_audiofile=temp_audio,
+        codec="libx264",
+    )
 
 if __name__ == "__main__":
     # main()
@@ -92,14 +97,21 @@ if __name__ == "__main__":
             movie_folder = f"{movie.temp_file}/{movie.title}"
             subtitle_clip = create_subtitles(movie_folder)
             movie_clip = VideoFileClip(f"{movie_folder}.mp4")
-            subtitled_movie = CompositeVideoClip([movie_clip, subtitle_clip])
+            subtitled_movie = CompositeVideoClip([
+                movie_clip, 
+                subtitle_clip.set_duration(movie_clip.duration)
+            ])
+            subtitled_movie.set_fps(movie_clip.fps)
+
             subtitle_threads.append(
                 threading.Thread(
                     target=write,
-                    args=(subtitled_movie, f"./{PRODUCTS_DIR}/{d}/{d}.mp4", None, 8, ),
+                    args=(subtitled_movie, f"{movie_folder}-subs.mp4", None, 8, ),
                 )
             )
 
+            os.remove(f"{movie_folder}.srt")
+            os.remove(f"{movie_folder}.mp4")
 
     for thread in subtitle_threads:
         thread.start()
